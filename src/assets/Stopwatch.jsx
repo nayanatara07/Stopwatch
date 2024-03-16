1bdd7cca7a3437d0ab4c809f432b986ebec7f156
nayanatara07 <nayana2017tara@gmail.com>
2024-03-16 21:33:01 +0530

    start.stop and reset buttons

--- src/assets/Stopwatch.jsx
+++ src/assets/Stopwatch.jsx
@@ -32,6 +32,11 @@ function Stopwatch(){
     return (
         <div className="stopwatch">
             <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <button onClick={start} className="start-button">Start</button>
+                <button onClick={stop} className="stop-button">Stop</button>
+                <button onClick={reset} className = "reset-button">Reset</button>
+            </div>
             
         </div>
     );
