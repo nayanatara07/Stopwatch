43c3d1b3da8eef893d63b89b0295328acb4620e9
nayanatara07 <nayana2017tara@gmail.com>
2024-03-17 02:54:53 +0530

    padStart method for Strings used for displaying 2 ele

--- src/assets/Stopwatch.jsx
+++ src/assets/Stopwatch.jsx
@@ -43,6 +43,10 @@ function Stopwatch(){
         let seconds = Math.floor(elapsedTime / (1000) % 60);
         let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
 
         return `${minutes}:${seconds}:${milliseconds}`;
 
