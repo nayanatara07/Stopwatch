5d96f4ed87fd502a7a7b68bac5534dafe2a0681d
nayanatara07 <nayana2017tara@gmail.com>
2024-03-16 21:21:19 +0530

    isRunning and setElapsedTime

---
+++ src/assets/Stopwatch.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function Stopwatch(){
+
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+        
+        }, [isRunning]);
+    
+    function start() {
+        
+    }
+
+        
+    function stop() {
+        
+    }
+
+        
+    function reset() {
+        
+    }
+
+    function formatTime() {
+        
+    }
+    
+    return(<></>);
+
+}
+export default Stopwatch
