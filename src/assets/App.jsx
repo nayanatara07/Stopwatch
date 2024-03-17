adb55014cb4cee277b977a34b1cdacca4075e2b8
nayanatara07 <nayana2017tara@gmail.com>
2024-03-17 15:29:09 +0530

    just few touch ups

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -2,10 +2,22 @@ import Stopwatch from "./Stopwatch";
 
 function App() {
 
-  return (<>
-    <Stopwatch/>
-    <Stopwatch/>
-    <Stopwatch/>
-    </>);
+  return (
+    <>
+        <div>
+            <span style={{ fontWeight: 'bold' }}>Stopwatch 1: Task A</span>
+            <Stopwatch />
+        </div>
+        <div>
+            <span style={{ fontWeight: 'bold' }}>Stopwatch 2: Task B</span>
+            <Stopwatch />
+        </div>
+        <div>
+            <span style={{ fontWeight: 'bold' }}>Stopwatch 3: Task C</span>
+            <Stopwatch />
+        </div>
+    </>
+);
+
 }
 export default App;
